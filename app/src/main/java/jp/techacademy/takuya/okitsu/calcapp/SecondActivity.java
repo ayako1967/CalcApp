4b5ec1b90ad702c6a1e48127816aa522cb181364
ayako1967 <takuya_kctn1414@tbz.t-com.ne.jp>
2016-10-29 17:50:51 +0900

    ボタン配置　完成 画面遷移　完成 前画面　　完成 後ろ画面　途中

--- app/src/main/java/jp/techacademy/takuya/okitsu/calcapp/SecondActivity.java
+++ app/src/main/java/jp/techacademy/takuya/okitsu/calcapp/SecondActivity.java
@@ -1,7 +1,9 @@
 package jp.techacademy.takuya.okitsu.calcapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        Intent intent = getIntent();
+        int value1 = intent.getIntExtra("VALUE1",0);
+        int value2 = intent.getIntExtra("VALUE2",0);
+
+        TextView textView = (TextView) findViewById(R.id.textView);
+        textView.setText(String.valueOf(value1 + value2));
     }
 }
