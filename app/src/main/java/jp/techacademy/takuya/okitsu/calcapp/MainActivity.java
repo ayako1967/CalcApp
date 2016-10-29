14081dbc34c3349de242dff89153dcda2ece6037
ayako1967 <takuya_kctn1414@tbz.t-com.ne.jp>
2016-10-29 16:22:22 +0900

    ボタン配置　完成 画面遷移　途中

--- app/src/main/java/jp/techacademy/takuya/okitsu/calcapp/MainActivity.java
+++ app/src/main/java/jp/techacademy/takuya/okitsu/calcapp/MainActivity.java
@@ -1,13 +1,33 @@
 package jp.techacademy.takuya.okitsu.calcapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button1 = (Button) findViewById(R.id.button1);
+        button1.setOnClickListener(this);
+
+        Button button2 = (Button) findViewById(R.id.button2);
+        button2.setOnClickListener(this);
+
+        Button button3 = (Button) findViewById(R.id.button3);
+        button3.setOnClickListener(this);
+
+        Button button4 = (Button) findViewById(R.id.button4);
+        button4.setOnClickListener(this);
+    }
+    @Override
+    public void onClick(View v){
+        Intent intent = new Intent(this,SecondActivity.class);
+        startActivity(intent);
     }
 }
