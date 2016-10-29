8226f361f35b793da073c3c52468190f2b3d9671
ayako1967 <takuya_kctn1414@tbz.t-com.ne.jp>
2016-10-29 17:11:05 +0900

    ボタン配置　完成 画面遷移　完成 前画面　　完成

--- app/src/main/java/jp/techacademy/takuya/okitsu/calcapp/MainActivity.java
+++ app/src/main/java/jp/techacademy/takuya/okitsu/calcapp/MainActivity.java
@@ -5,13 +5,21 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+
+     private EditText text1 ;
+     private EditText text2 ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+         text1 = (EditText) findViewById(R.id.text1);
+         text2 = (EditText) findViewById(R.id.text2);
 
         Button button1 = (Button) findViewById(R.id.button1);
         button1.setOnClickListener(this);
@@ -28,6 +36,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View v){
         Intent intent = new Intent(this,SecondActivity.class);
+
+        intent.putExtra("VALUE1",text1.getText().toString());
+        intent.putExtra("VALUE2",text2.getText().toString());
         startActivity(intent);
     }
 }
